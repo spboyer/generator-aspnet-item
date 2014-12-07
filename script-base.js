75d827f263bc7d9eb6686f7b274e293bac45c94e
Shayne Boyer <shayne.p.boyer@disney.com>
2014-12-07 11:08:00 -0500

    cleanup

--- script-base.js
+++ script-base.js
@@ -11,15 +11,6 @@ var NamedGenerator = module.exports = function NamedGenerator() {
   this.sourceRoot(path.join(__dirname, sourceRoot));
 };
 
-
-//var Generator = module.exports = function Generator() {
-//  yeoman.generators.Base.apply(this, arguments);
-
-//  var sourceRoot = '/templates/';
-//  this.sourceRoot(path.join(__dirname, sourceRoot));
-//}; 
-
-//util.inherits(Generator, yeoman.generators.Base);
 util.inherits(NamedGenerator, yeoman.generators.NamedBase);
 
 NamedGenerator.prototype.generateTemplateFile = function(templateFile, targetFile, templateData) {
@@ -33,11 +24,3 @@ NamedGenerator.prototype.generateTemplateFile = function(templateFile, targetFil
 
   this.log(targetFile + ' created.')
 }
-
-//Generator.prototype.generateStandardFile = function(sourceFile, targetFile){
-//   this.log('You called the aspnet subgenerator with the arg ' + sourceFile);
-
-//  this.src.copy(sourceFile, targetFile);
-
-//  this.log(targetFile + ' created.')
-//}
